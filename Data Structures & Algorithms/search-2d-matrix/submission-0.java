d649fb2179705380035fdcc966084c9c4445e032
Andrii Liashenko <andrii.liashenko@datadoghq.com>
2026-07-05 21:26:06 +0200

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // binary search
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int l = 0, r = n*m-1;
+        while (l <= r) {
+            int mid = l+(r-l)/2;
+            int row = mid/m;
+            int col = mid%m;
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        return false;
+    }
+}
