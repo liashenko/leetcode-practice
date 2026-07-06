ef2990785eb457c251070e4bfb20835729d0c33d
Andrii Liashenko <andrii.liashenko@datadoghq.com>
2026-07-06 11:13:09 +0200

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.java
@@ -0,0 +1,41 @@
+class TimeMap {
+
+    Map<String, List<Integer>> timestamps = new HashMap<>();
+    Map<String, List<String>> values = new HashMap<>();
+
+    public TimeMap() {
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        // key -> list<timestamp>
+        // key -> list<value>
+        timestamps.computeIfAbsent(key, k -> new ArrayList<>())
+            .add(timestamp);
+        values.computeIfAbsent(key, k -> new ArrayList<>())
+            .add(value);
+    }
+    
+    public String get(String key, int timestamp) {
+        // find upper bound t > timestamp
+        // return id[t]-1 or "" if id=0
+        if (!timestamps.containsKey(key)) {
+            return "";
+        }
+        List<Integer> keyTimestamps = timestamps.get(key);
+        int upperBound = findUpperBound(keyTimestamps, timestamp);
+        return upperBound == 0 ? "" : values.get(key).get(upperBound-1);
+    }
+
+    private int findUpperBound(List<Integer> v, int t) {
+        int l = 0, r = v.size();
+        while (l < r) {
+            int mid = l + (r-l)/2;
+            if (v.get(mid) > t) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
